8c60170cc08b467f45597258160e7eebb08b1734
Hyma2004 <hymatamarana2000@gmail.com>
2024-04-18 15:13:12 +0000

    Conditional Rendering LightDarkMode

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LightDarkMode extends Component {
+  state = {isDarkMode: true}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
+  }
+
+  render() {
+    const {isDarkMode} = this.state
+    const modeClassName = isDarkMode ? 'Dark Mode' : 'Light Mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
+    return (
+      <div className="container">
+        <div className={`change-container ${modeClassName}`}>
+          <h1 className="heading">Click to Change Mode</h1>
+          <button className="button" type="button" onClick={this.onClickButton}>
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default LightDarkMode
+// Write your code here
